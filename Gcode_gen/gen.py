0a08d92a77cf260ce5b6c9ffb4cffff776e6419f
Jeremy Dudo <jmdudo2@gmail.com>
2018-12-06 08:11:55 -0500

    more function

--- Gcode_gen/gen.py
+++ Gcode_gen/gen.py
@@ -2,7 +2,7 @@ import numpy as np
 import datetime
 
 class syringe():
-    def __init__(self):
+    def __init__(self, text=True):
         """
         Note: not modular yet, setting constants for now!
 
@@ -29,14 +29,22 @@ class syringe():
         self.in_tube = 50       # mm
 
         self.pos = [0, 0, self.resting]
+        self.pos_WashVial = [10, 10] # only need x,y coordinates (these are made up atm)
 
         self.max_volume = 1     # ml
         self.dist_to_vol = 57   # mm/ml 
+        self.max_plunger_location = -self.max_volume*self.dist_to_vol
+        self.plunger_location = 0
 
         self.max_speed = 10     # temp, need to fix
         self.max_pump_rate = 10 # temp
 
-        file = open("gcode.txt", "w") # constant override, everytime
+        if text:
+            self.filename = "gcode.txt"
+        else:
+            self.filename = "gcode.gcode"
+
+        file = open(self.filename, "w") # constant override, everytime
         file.close() 
 
 
@@ -55,25 +63,37 @@ class syringe():
         clear_height = "G0 Z"       # need to define where 'home' is on the Z axis
         
         # move to next location
-        g_string = "G1 X{0} Y{1} E{2} F2000".format(x_cor, y_cor) # speed is set as const.
+        g_string = "G1 X{0} Y{1} F2000".format(x_cor, y_cor) # speed is set as const.
 
         # move into vial
         into_vial = "G1 Z{} F2000".format(self.in_tube)
 
         
 
-        file = open("gcode.txt", "a")
+        file = open(self.filename, "a")
         file.write(clear_height+ '\n')
         file.write(g_string+ "\n")
         file.write(into_vial+ "\n")
 
         file.close()
 
+
     def write_pump(self, pump):
         """
         pump: mL pushed[pos]/pulled[neg]
         """
         extrude = pump * self.dist_to_vol
+
+        self.plunger_location += extrude
+
+        if self.plunger_location > 0:
+            print("Plunger more than empty")
+            raise Warning
+
+        g_string = "G1 E{}".format(extrude)
+
+        file = open(self.filename, "a")
+        file.write(g_string+"\n")
 
 
     def read_script(self, locations):
